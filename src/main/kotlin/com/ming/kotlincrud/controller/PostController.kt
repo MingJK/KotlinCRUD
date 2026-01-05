6c5c1c7a0a0cd8d26d11610716d09749bcad9449
Mingjk <s24019@gsm.hs.kr>
2026-01-05 20:19:32 +0900

    add: update 기능 추가

--- src/main/kotlin/com/ming/kotlincrud/controller/PostController.kt
+++ src/main/kotlin/com/ming/kotlincrud/controller/PostController.kt
@@ -5,6 +5,7 @@ import com.ming.kotlincrud.service.PostService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
@@ -25,5 +26,11 @@ class PostController
     @GetMapping("/delete/{id}")
     fun delete(@PathVariable("id") id: Long) {
         return postService.deleteById(id)
+    }
+
+    @PutMapping("/update/{id}")
+    fun update(@PathVariable("id") id: Long, @RequestBody post: Post): Post {
+        post.postId = id
+        return postService.save(post)
     }
 }
